e90fbb09835705fedc9bc6ddfb690dec50be3375
Julia Fuhrman <JulNicole@gmail.com>
2018-09-12 17:57:56 -0400

    fix: Submission corrections

--- js/main.js
+++ js/main.js
@@ -176,7 +176,7 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -191,6 +191,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label', `View details for ${restaurant.name}`);
   li.append(more)
 
   return li
